a7412c952e15e7797872adae4065aae4ba08ebe6
jyotsnabali <49472813+jyotsnabali@users.noreply.github.com>
2026-04-08 21:58:23 +1000

    Initial commit for distributed tracing demo

---
+++ Controllers/ProcessController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendApi.Controllers;
+
+[ApiController]
+[Route("process")]
+public class ProcessController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Backend processed request");
+    }
+}
